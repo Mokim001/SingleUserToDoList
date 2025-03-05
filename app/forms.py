7a6bc7376cad06421fb47d647376dad1ca5a57d5
unknown <mokimcodes@gmail.com>
2025-03-05 18:21:10 +0530

    final

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,19 @@
-from django import forms
-from .models import *
+# from django import forms
+# from .models import *
 
-class UserForm(forms.ModelForm):
-    class Meta:
-        model=User 
-        fields=['first_name', 'last_name', 'email', 'username', 'password']
-        help_texts={'username': ''}
+# class UserForm(forms.ModelForm):
+#     class Meta:
+#         model=User 
+#         fields=['first_name', 'last_name', 'email', 'username', 'password']
+#         help_texts={'username': ''}
 
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model=Profile
-        exclude=['username']
+# class ProfileForm(forms.ModelForm):
+#     class Meta:
+#         model=Profile
+#         exclude=['username']
 
 
-class TodoForm(forms.ModelForm):
-    class Meta:
-        model=Todo
-        exclude=['ussername']
+# class TodoForm(forms.ModelForm):
+#     class Meta:
+#         model=Todo
+#         exclude=['ussername']
--- app/models.py
+++ app/models.py
@@ -3,16 +3,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 #smaraki models is ur job and forms too
-class Profile(models.Model):
-    username=models.ForeignKey(User, on_delete=models.CASCADE)
-    pno=models.CharField(max_length=100)
+# class Profile(models.Model):
+#     username=models.ForeignKey(User, on_delete=models.CASCADE)
+#     pno=models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.username.username
+#     def __str__(self):
+#         return self.username.username
     
 class Todo(models.Model):
-    username=models.ForeignKey(User, on_delete=models.CASCADE)
-    Sl_No=models.IntegerField(primary_key=True)
+    # username=models.ForeignKey(User, on_delete=models.CASCADE)
+    # Sl_No=models.IntegerField(primary_key=True)
     title=models.CharField(max_length=100)
     desc=models.CharField(max_length=250)
 
