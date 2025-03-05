127ab8c7465b5b1b6090fa7ace4673f3600a52e9
Smaraki-Dash <smarakidash1003@gmail.com>
2025-03-05 15:34:25 +0530

    model created

--- app/models.py
+++ app/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 #smaraki models is ur job and forms too
+class Profile(models.Model):
+    username=models.ForeignKey(User, on_delete=models.CASCADE)
+    pno=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username.username
+    
+class Todo(models.Model):
+    username=models.ForeignKey(User, on_delete=models.CASCADE)
+    Sl_No=models.IntegerField(primary_key=True)
+    title=models.CharField(max_length=100)
+    desc=models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title[:12]
